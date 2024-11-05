b243666c2d7aa922d0a5bc5ef6894380a0f78da3
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-11-05 17:11:56 -0300

    Correção do anotation e verificação do endPoint

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,14 +1,17 @@
 package com.devsuperior.dscommerce.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping(name = "/products")
+@RequestMapping(value = "/products")
 public class ProductController {
 
-
-
+@GetMapping
+public String teste(){
+    return "Olá mundo!";
+}
 
 
 }
