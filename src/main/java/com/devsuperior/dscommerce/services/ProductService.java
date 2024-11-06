22c4d442c4dae934a99fb69d26bd5aea03829119
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-11-06 16:03:03 -0300

    A dependencia ProductRepository para acessar o banco de dados

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -2,6 +2,7 @@ package com.devsuperior.dscommerce.services;
 
 
 import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.entities.Product;
 import com.devsuperior.dscommerce.repositories.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
