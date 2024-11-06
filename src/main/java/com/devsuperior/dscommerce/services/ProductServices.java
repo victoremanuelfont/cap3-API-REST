1674232de97b737cdf332696e674c91c96c335cf
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-11-06 09:21:24 -0300

    Obedecendo a arquitetura

--- src/main/java/com/devsuperior/dscommerce/services/ProductServices.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductServices.java
@@ -1,8 +1,15 @@
 package com.devsuperior.dscommerce.services;
 
 
+import com.devsuperior.dscommerce.dto.ProductDTO;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProductServices {
+
+    public ProductDTO findById(Long id){
+    }
+
+
+
 }
