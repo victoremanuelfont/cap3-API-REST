57b46ac6845c64681267d5e72bf2a1aaf0559bee
Victor Emanuel <victoremanuelfont3@gmail.com>
2024-11-06 15:54:18 -0300

    Corrigi o nome da classe >ProductService<

---
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -0,0 +1,21 @@
+package com.devsuperior.dscommerce.services;
+
+
+import com.devsuperior.dscommerce.dto.ProductDTO;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public ProductDTO findById(Long id){
+
+    }
+
+
+
+}
